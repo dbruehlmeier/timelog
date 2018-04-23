15c9d59abf6fb8dcfe14a7163563aac9a6a36de0
dbruehlmeier <31269608+dbruehlmeier@users.noreply.github.com>
2018-04-23 19:15:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ var taskDropdown = [];
 $.getJSON( "https://time.villageoffice.ch/zoho-api/portal/villageoffice/projects/?authtoken=bf97913da8a83b9bbccaa87e66242727&status=active", function( projectArr ) {
   projectArr.projects.forEach(function(projectEntry) {
     $.getJSON( "https://time.villageoffice.ch/zoho-api/portal/villageoffice/projects/"+projectEntry.id+"/tasks/?authtoken=bf97913da8a83b9bbccaa87e66242727&owner=20062563695&status=all&time=all&priority=all", function( taskArr ) {
-      // TODO
+      JSON.stringify(taskArr);
     });
   });
 });
