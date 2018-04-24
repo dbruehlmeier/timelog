f2beb425c1ab5f76ddd6b736558da8b06ea6492e
dbruehlmeier <31269608+dbruehlmeier@users.noreply.github.com>
2018-04-24 07:28:47 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ $.getJSON( "https://time.villageoffice.ch/zoho-api/portal/villageoffice/projects
     alert(taskUrl);
     $.getJSON( taskUrl, function( taskArr ) {
       alert(JSON.stringify(taskArr));
-    }).done()
+    }).done(function(){alert("done");})
   });
 });
 
